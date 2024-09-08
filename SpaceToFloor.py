92980a5f1161a3f83763b2df4ee4c343b29428c4
pawel-kokoszka <83219154+pawel-kokoszka@users.noreply.github.com>
2024-09-09 01:57:25 +0200

    Added main loop to replace space to us

--- SpaceToFloor.py
+++ SpaceToFloor.py
@@ -1,2 +1,29 @@
+import os
 
-print("hello world")
+folder_path = 'G:/renametest'
+
+files = os.listdir(folder_path)
+
+only_files = []
+
+for f in files:
+    if os.path.isfile(os.path.join(folder_path, f)) and  " " in f:
+        only_files.append(f)
+
+files = only_files
+
+
+for filename in files:
+    
+    
+        
+    new_name = filename.replace(' ', '_')
+    
+    
+    old_file = os.path.join(folder_path, filename)
+    new_file = os.path.join(folder_path, new_name)
+    
+    
+    os.rename(old_file, new_file)
+
+    print(f"Renamed: {filename} -> {new_name}")
